859a951cb59cedb5d9ca35d68d290ba1bf287093
Tiago S. Hucs <tiagohucs@gmail.com>
2025-06-30 08:31:22 -0300

    iniciando teste

--- src/test/java/br/com/alura/service/AbrigoServiceTest.java
+++ src/test/java/br/com/alura/service/AbrigoServiceTest.java
@@ -1,13 +1,21 @@
 package br.com.alura.service;
 
+import br.com.alura.client.HttpService;
+import br.com.alura.dominio.Abrigo;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.net.http.HttpResponse;
 
 class AbrigoServiceTest {
 
+    private HttpService mockClient = Mockito.mock(HttpService.class);
+    private AbrigoService abrigoService = new AbrigoService(mockClient);
+    private HttpResponse<String> mockResponse = Mockito.mock(HttpResponse.class);
+    private Abrigo abrigo = new Abrigo("","","");
+
     @Test
-    void cadastrarAbrigo() {
+    void verificaSe() {
     }
 
     @Test
